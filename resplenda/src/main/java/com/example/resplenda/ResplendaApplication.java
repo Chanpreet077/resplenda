4dde51262b8aa98a84c1021e2c625edf4fe85c78
Chanpreet Makkar <1makkarkhu@gmail.com>
2025-07-13 20:41:22 -0400

    Initial commit - setup Spring Boot project

---
+++ resplenda/src/main/java/com/example/resplenda/ResplendaApplication.java
@@ -0,0 +1,13 @@
+package com.example.resplenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResplendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResplendaApplication.class, args);
+	}
+
+}
---
+++ resplenda/src/test/java/com/example/resplenda/ResplendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resplenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResplendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
